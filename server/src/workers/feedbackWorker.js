97b7da6a43abbb408ade42a7e24de93048eb5ff3
Jayesh Sharma <jayesh2005sharma@gmail.com>
2026-06-11 00:10:06 +0530

    Solving the feedbackWorkers process

--- server/src/workers/feedbackWorker.js
+++ server/src/workers/feedbackWorker.js
@@ -2,8 +2,10 @@ import client from "../config/redis";
 import prisma from "../config/db";
 import { solveIssue } from "../services/geminiServices";
 
-export const startWorker = () => {
-    setInterval(async () => {
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+export const startWorker = async () => {
+    while(true){
         const data = await client.zRange(
             "feedbackQueue",
             0,
@@ -14,21 +16,24 @@ export const startWorker = () => {
         )
         if (data.length === 0) {
             console.log("Queue Empty");
-            return
+            await sleep(5000);
+            continue
         }
         const feedbackId = data[0];
+        await client.zRem("feedbackQueue", data[0])
         const feedback = await prisma.feedback.findUnique({
             where: {
                 id: feedbackId
             }
         });
+        
         console.log("Processing : ", feedback.title);
 
 
         /* AI solves the issue */
         const content = feedback.content;
         const solution = await solveIssue(content);
-        console.log(solution);
+        console.log(solution);   
         await prisma.feedback.update({
             where: {
                 id: feedback.id
@@ -37,8 +42,8 @@ export const startWorker = () => {
                 status: "resolved"
             }
         });
-        await client.zRem("feedbackQueue", data[0])
-    },5000)
+        
+    }
 }
 
 
