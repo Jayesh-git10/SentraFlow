a2f9e0295450cb8bc03b827f5915b28f7579f3a2
Jayesh Sharma <jayesh2005sharma@gmail.com>
2026-06-11 23:50:02 +0530

    userLogin function is made

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -1,9 +1,16 @@
 import prisma from "../config/db.js";
+import bcrypt from "bcrypt"
 
 export const userRegister = async (req, res) => {
   try {
-    const { name, email } = req.body;
+    const { name, email, password } = req.body;
 
+    if(!name || !email || !password){
+      return res.status(400).json({
+        success:false,
+        message : "All feilds are reqquired"
+      })
+    }
     // Check if user already exists
     const existingUser = await prisma.user.findUnique({
       where: {
@@ -19,16 +26,20 @@ export const userRegister = async (req, res) => {
     }
 
     // Create user
+    const hashedPassword = await bcrypt.hash(password , 10);
     const user = await prisma.user.create({
       data: {
         name,
         email,
+        password : hashedPassword
       },
     });
 
+    const {hashedPassword:_,...safeUser} = user; 
+
     res.status(201).json({
       success: true,
-      user,
+      user : safeUser,
     });
 
   } catch (error) {
@@ -41,24 +52,49 @@ export const userRegister = async (req, res) => {
 
 export const userLogin = async (req,res)=>{
   try {
-    const {user , email} = req.body();
+    const {email , password} = req.body;
+
+    if(!email || !password){
+      return res.status(400).json({
+        success:false,
+        message : "All feilds are reqquired"
+      })
+    }
+
     const existingUser = await prisma.user.findUnique({
       where : {
         email,
       }
     })
+
     if(!existingUser){
-      res.status(400).json({
+      return res.status(400).json({
         success : false,
-        message : "User not found"
+        message : "User not found ! Register First"
       })
     }
-    const user = await prisma.user.create({
-      data:{
-        name,
-        email
+
+    const isMatch = await bcrypt.compare(
+      password,
+      existingUser.password
+    )
+
+    if(!isMatch){
+      return res.status(400).json({
+        success : false,
+        message : "Wrong password"
+      })
+    }
+    return res.status(200).json({
+      success : true,
+      message : "Login Successful",
+      user:{
+        id : existingUser.id,
+        name : existingUser.name,
+        email : existingUser.email
       }
     })
+
   } catch (error) {
     res.status(500).json({
       success : false,
