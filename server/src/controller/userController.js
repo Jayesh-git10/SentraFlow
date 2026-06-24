0de887ac0073c2068587165e8ee8185d7a7deaa9
Jayesh Sharma <jayesh2005sharma@gmail.com>
2026-06-24 12:25:33 +0530

    Update userController.js

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -91,7 +91,7 @@ export const userLogin = async (req,res)=>{
 
     res.cookie("token",token,{
       httpOnly : true,
-      secure : process.env.NODE_ENV === 'production',
+      secure : true,
       sameSite: "none",
       maxAge : 7*24*60*60*1000
     })
